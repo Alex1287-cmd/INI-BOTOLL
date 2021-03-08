a33f89ca890935bd8417995054d1e98c87787039
Alex1287-cmd <75934725+Alex1287-cmd@users.noreply.github.com>
2021-03-08 20:02:14 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • SMART[SMARTFREENNN][08881393308]
+│ • Telkomsel []
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
